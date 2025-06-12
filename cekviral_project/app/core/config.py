61e21b6dc15e8566aee259f2c9e6eae64c787ab7
Syahel Razaba <syahel.razaba@gmail.com>
2025-06-12 10:59:22 +0700

    update config.py

--- cekviral_project/app/core/config.py
+++ cekviral_project/app/core/config.py
@@ -1,17 +1,20 @@
-# cekviral_project/app/core/config.py
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# app/core/config.py
+from pydantic_settings import BaseSettings
+import os
+
+# Khusus untuk lokal saja
+if os.getenv("ENV", "local") == "local":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 class Settings(BaseSettings):
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
+
     PROJECT_NAME: str = "CekViral API"
     PROJECT_VERSION: str = "0.1.0"
     MODEL_PATH: str = "models/"
     YDL_TEMP_DIR: str = "temp_downloads/"
-    GCP_CREDENTIALS_PATH: str | None = None # Path ke file kredensial GCP
-
-    # Variabel untuk kredensial Supabase
-    SUPABASE_URL: str | None = None
-    SUPABASE_KEY: str | None = None
-    
-    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
+    GCP_CREDENTIALS_PATH: str | None = None
 
 settings = Settings()
